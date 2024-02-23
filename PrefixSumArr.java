465dcd7c3fae28817d10cda1960f7d7c309b628c
chavasanth <vasanthchelpaka@gmail.com>
2024-02-24 00:06:35 +0530

    covering Edge cases

--- PrefixSumArr.java
+++ PrefixSumArr.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class PrefixSumArr {
     public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter range:");
+        int a = obj.nextInt();
+        int b = obj.nextInt();
     int arr[] = new int[]{1,2,3,4,5,6,7,8,9};
     for(int i=1;i<arr.length;i++){
         arr[i] = arr[i-1] + arr[i];
+        if(b == i) break;
     } 
-    for(int j=0; j<arr.length; j++){
-        System.out.print(arr[j] + ",");
+    int x=0;
+    if(a != 0){
+     x = arr[b]-arr[a];
+    }else
+    {
+        x=arr[b];
     }
+    System.out.println("a:" + a);
+    System.out.println("b:" + b);
+    System.out.println("x:" + x);
 }
 }
